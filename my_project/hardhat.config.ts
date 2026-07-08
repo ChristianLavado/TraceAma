34c313127e7e39818e61901a6e01aedfa174f14a
ChristianLavado <christian.lavadov@unmsm.edu.pe>
2026-07-07 19:42:25 -0500

    Create hardhat.config.ts

---
+++ my_project/hardhat.config.ts
@@ -0,0 +1,10 @@
+// Configuración de red para TraceAma
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    polygonAmoy: {
+      url: "https://rpc-amoy.polygon.technology",
+      chainId: 80002,
+    }
+  }
+};
